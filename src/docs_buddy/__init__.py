330831f8f90860df8d305e40039f660b0400302e
Kenny Rch <ken@99nth.com>
2026-02-26 17:48:12 +0300

    fix: remove unused imports from root package

--- src/docs_buddy/__init__.py
+++ src/docs_buddy/__init__.py
@@ -8,14 +8,3 @@ Major functionality
 - Indexing the documentation
 - Answer queries from the documentation
 """
-
-from dataclasses import dataclass, asdict
-import subprocess
-from pathlib import Path
-from urllib.parse import urlparse
-import json
-import shutil
-import os
-from typing import Union, Protocol, Iterator
-
-
